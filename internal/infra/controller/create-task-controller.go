3335617f323611a38cdee5d158a288fc6c97149f
Kayo Oliveira <g.kayooliveira123@gmail.com>
2024-01-07 20:05:37 -0300

    feat(Creating CreateTaskController): :sparkles:

---
+++ internal/infra/controller/create-task-controller.go
@@ -0,0 +1,36 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/kayooliveira/dinodo-api-go/internal/entity"
+	"github.com/kayooliveira/dinodo-api-go/internal/infra/repository"
+)
+
+type TaskRequestBody struct {
+	Task     string
+	Finished bool
+}
+
+func CreateTaskController(ctx *gin.Context) {
+
+	var taskRequestBody TaskRequestBody
+
+	err := ctx.BindJSON(&taskRequestBody)
+
+	repository := repository.NewTaskRepositoryMysql(db)
+
+	task := entity.Task{
+		Task:     taskRequestBody.Task,
+		Finished: taskRequestBody.Finished,
+	}
+
+	repository.Create(&task)
+
+	if err != nil {
+		sendError(ctx, 400, "BAD REQUEST")
+		return
+	}
+
+	sendSuccess(ctx, "create-task", task)
+
+}
