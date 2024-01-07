3cca9274a7dbac6e27e8ba8606232aa15b7aa249
Kayo Oliveira <g.kayooliveira123@gmail.com>
2024-01-07 20:05:12 -0300

    feat(Creating response helper to controller package): :sparkles:

---
+++ internal/infra/controller/response.go
@@ -0,0 +1,24 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func sendSuccess(ctx *gin.Context, operation string, data interface{}) {
+	ctx.Header("Content-type", "application/json")
+	ctx.JSON(http.StatusOK, gin.H{
+		"message":   "Success",
+		"operation": operation,
+		"data":      data,
+	})
+}
+
+func sendError(ctx *gin.Context, code int, message string) {
+	ctx.Header("Content-type", "application/json")
+	ctx.JSON(http.StatusOK, gin.H{
+		"message":   message,
+		"errorCode": code,
+	})
+}
