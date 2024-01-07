ce07bcf960662b266ead62a1ab6aa46044b19881
Kayo Oliveira <g.kayooliveira123@gmail.com>
2024-01-07 20:03:45 -0300

    feat(Creating Task Repository with Create, Update, Get, GetAll and Delete methods): :sparkles:

---
+++ internal/infra/repository/task-repository-mysql.go
@@ -0,0 +1,68 @@
+package repository
+
+import (
+	"github.com/kayooliveira/dinodo-api-go/internal/entity"
+	"gorm.io/gorm"
+)
+
+type TaskRepositoryMysql struct {
+	DB *gorm.DB
+}
+
+func NewTaskRepositoryMysql(db *gorm.DB) *TaskRepositoryMysql {
+	return &TaskRepositoryMysql{DB: db}
+}
+
+func (r *TaskRepositoryMysql) Create(task *entity.Task) error {
+	result := r.DB.Create(&task)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func (r *TaskRepositoryMysql) Update(task *entity.Task) error {
+	result := r.DB.Save(&task)
+
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func (r *TaskRepositoryMysql) Get(id string) (*entity.Task, error) {
+	task := entity.Task{}
+	result := r.DB.First(&task, id)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &task, nil
+}
+
+func (r *TaskRepositoryMysql) GetAll() ([]*entity.Task, error) {
+	tasks := []*entity.Task{}
+
+	result := r.DB.Find(&tasks)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return tasks, nil
+}
+
+func (r *TaskRepositoryMysql) Delete(id string) error {
+	task := entity.Task{}
+
+	result := r.DB.Find(&task, id)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	r.DB.Delete(task)
+
+	return nil
+}
