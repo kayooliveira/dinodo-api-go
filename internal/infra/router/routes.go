2cbcb1bb89dffcce9742722a532e78f4dbc60727
Kayo Oliveira <g.kayooliveira123@gmail.com>
2024-01-08 19:07:39 -0300

    feat(Adding new routes to update and get one task): :sparkles:

--- internal/infra/router/routes.go
+++ internal/infra/router/routes.go
@@ -11,6 +11,8 @@ func initRoutes(router *gin.RouterGroup) {
 	basePath := "/api/v1"
 	v1 := router.Group(basePath)
 
-	v1.GET("/tasks", controller.GetAllTasksController)
 	v1.POST("/tasks", controller.CreateTaskController)
+	v1.GET("/tasks/:id", controller.GetOneTaskController)
+	v1.GET("/tasks", controller.GetAllTasksController)
+	v1.PUT("/tasks/:id", controller.UpdateTaskController)
 }
