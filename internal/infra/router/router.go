0d4a16e742eb3dcdc304e0d890db2ecd41117145
Kayo Oliveira <g.kayooliveira123@gmail.com>
2024-01-08 19:43:02 -0300

    feat(Now app port is setted on .env file): :sparkles:

--- internal/infra/router/router.go
+++ internal/infra/router/router.go
@@ -1,7 +1,9 @@
 package router
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,8 +13,10 @@ func InitRouter() {
 	router := gin.Default()
 
 	initRoutes(&router.RouterGroup)
+	port := os.Getenv("PORT")
+	appUrl := fmt.Sprintf("127.0.0.1:%s", port)
 
-	if err := router.Run("127.0.0.1:4000"); err != nil {
+	if err := router.Run(appUrl); err != nil {
 		log.Fatal(err)
 	}
 }
