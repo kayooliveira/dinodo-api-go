d05b5780024f3ff2dd21882fb5683b6dab37ceeb
Kayo Oliveira <g.kayooliveira123@gmail.com>
2024-01-09 23:01:46 -0300

    feat(Creating auth package to GenerateUserToken and ParseUserToken): :sparkles:

---
+++ internal/infra/auth/auth.go
@@ -0,0 +1,58 @@
+package auth
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type TokenPayload struct {
+	jwt.RegisteredClaims
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Email       string `json:"email"`
+	AccessToken string `json:"accessToken"`
+}
+
+var tokenSecret = os.Getenv("JWT_SECRET")
+
+func GenerateUserToken(id string, name string, email string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, TokenPayload{
+		RegisteredClaims: jwt.RegisteredClaims{
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)),
+		},
+		ID:    id,
+		Name:  name,
+		Email: email,
+	})
+
+	signedString, err := token.SignedString([]byte(tokenSecret))
+
+	if err != nil {
+		return "", err
+	}
+
+	return signedString, nil
+}
+
+func ParseUserToken(token string) error {
+
+	var tokenPayload TokenPayload
+
+	parsed, err := jwt.ParseWithClaims(token, &tokenPayload, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if !parsed.Valid {
+		return fmt.Errorf("Invalid token")
+	}
+
+	return nil
+}
