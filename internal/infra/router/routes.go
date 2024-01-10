f275af4f36cd888cdf717a995b3e6fc845122a50
Kayo Oliveira <g.kayooliveira123@gmail.com>
2024-01-10 00:53:18 -0300

    feat(Implementing new routes to Create and Get a user by ID): :sparkles:

--- internal/infra/router/routes.go
+++ internal/infra/router/routes.go
@@ -3,6 +3,7 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/kayooliveira/dinodo-api-go/internal/infra/controller"
+	"github.com/kayooliveira/dinodo-api-go/internal/infra/middleware"
 )
 
 func initRoutes(router *gin.RouterGroup) {
@@ -20,4 +21,8 @@ func initRoutes(router *gin.RouterGroup) {
 	v1.PUT("/tasks/:id", controller.UpdateTaskController) // Update a task by ID
 
 	v1.DELETE("/tasks/:id", controller.DeleteTaskController) // Delete a task
+
+	v1.POST("/auth/signin", controller.CreateUserController) // Register a new User
+
+	v1.GET("/users/:id", middleware.AuthMiddleware, controller.GetUserByIdController) // Register a new User
 }
