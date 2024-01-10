a88ec0b4340e89177649a7f0f95593c84f26a908
Kayo Oliveira <g.kayooliveira123@gmail.com>
2024-01-09 23:44:02 -0300

    feat(Adding password crypto on BeforeCreate from user entity): :sparkles:

--- internal/domain/entity/user.go
+++ internal/domain/entity/user.go
@@ -4,15 +4,16 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
 type User struct {
 	ID        uuid.UUID       `json:"id" gorm:"type:char(36);primary_key"`
-	Username  string          `json:"username"`
+	Username  string          `json:"username" gorm:"unique"`
 	Name      string          `json:"name"`
-	Email     string          `json:"email"`
-	Password  string          `json:"password"`
+	Email     string          `json:"email" gorm:"unique"`
+	Password  string          `json:"-"`
 	Tasks     []Task          `json:"tasks"`
 	CreatedAt time.Time       `json:"createdAt"`
 	UpdatedAt *time.Time      `json:"updatedAt"`
@@ -21,14 +22,23 @@ type User struct {
 
 type UserRepository interface {
 	Create(user *User) error
-	Update(user *User) error
-	Get(id string) (*User, error)
-	GetAll() ([]*User, error)
-	Delete(user *User) error
+	GetById(id string) (*User, error)
+	// Update(user *User) error
+	// Get(id string) (*User, error)
+	// GetAll() ([]*User, error)
+	// Delete(user *User) error
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
 
 	u.ID = uuid.New()
+
+	password, err := bcrypt.GenerateFromPassword([]byte(u.Password), 14)
+	if err != nil {
+		return err
+	}
+
+	u.Password = string(password)
+
 	return
 }
